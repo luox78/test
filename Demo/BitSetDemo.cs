ecfaca78093789d00d7152891c9925624defd979
luox78 <1023354041@qq.com>
2021-09-21 14:23:28 +0800

    Add BitSet and BloomFilter demo

---
+++ Demo/BitSetDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace Demo
+{
+    public static class BitSetDemo
+    {
+        public static void New()
+        {
+            var bs = new BitArray(32 * 1000);
+            //set index 1 mark true
+            bs.Set(1, true);
+            Console.WriteLine($"Bitset index 1 is marked {bs.Get(1)}");
+
+            Console.WriteLine($"Bitset index 2 is marked {bs.Get(2)}");
+        }
+    }
+}
---
+++ Demo/BloomFilter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    public class BloomFilter
+    {
+        class InternalHash
+        {
+            private readonly int _cap;
+            private readonly int _seed;
+
+            public InternalHash(int cap, int seed)
+            {
+                _cap  = cap;
+                _seed = seed;
+            }
+
+            public int Hash(string str)
+            {
+                return 1;
+            }
+        }
+
+        private readonly BitArray _bitArray;
+        private readonly List<InternalHash> _funcs;
+
+        public BloomFilter(int cap, int funs)
+        {
+            _funcs = new List<InternalHash>();
+            for (int i = 0; i < funs; i++)
+            {
+                _funcs.Add(new InternalHash(cap, i));
+            }
+
+            _bitArray = new BitArray(cap);
+        }
+
+        public void Add(string str)
+        {
+            foreach (var internalHash in _funcs)
+            {
+                _bitArray.Set(internalHash.Hash(str), true);
+            }
+        }
+
+        public bool Exist(string str)
+        {
+            var result = true;
+            foreach (var internalHash in _funcs)
+            {
+                if (!_bitArray.Get(internalHash.Hash(str)))
+                {
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+    }
+}
